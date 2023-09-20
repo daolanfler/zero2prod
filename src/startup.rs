377c823fb3965a41d475697d1896b856d9d2bac6
daolanfler <daolanfler@gmail.com>
2023-09-21 00:08:32 +0800

    wip: the web:Data extractor

--- src/startup.rs
+++ src/startup.rs
@@ -1,11 +1,21 @@
 use crate::routes::{health_check, subscribe};
-use actix_web::{dev::Server, App, HttpServer};
+use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
-        .listen(listener)?
-        .run();
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // web::Data wraps our connection in an Atomic Reference Counted pointer (ARC)
+    let connection = web::Data::new(connection);
+
+    let server = HttpServer::new(move || {
+        App::new()
+            .service(health_check)
+            .service(subscribe)
+            // Get a pointer copy and attach it to the application state
+            .app_data(connection.clone())
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
