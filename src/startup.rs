bb29e61c53656d94e337a241085cab4640b1762e
daolanfler <daolanfler@gmail.com>
2023-09-21 22:26:52 +0800

    add tracing-aware logger

--- src/startup.rs
+++ src/startup.rs
@@ -1,8 +1,8 @@
 use crate::routes::{health_check, subscribe};
-use actix_web::middleware::Logger;
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // web::Data wraps our connection in an Atomic Reference Counted pointer (ARC)
@@ -10,7 +10,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
 
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .service(health_check)
             .service(subscribe)
             // Get a pointer copy and attach it to the application state
