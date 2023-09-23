badea78ba04caff47eff0228df826b0a6ca7c1b2
daolanfler <daolanfler@gmail.com>
2023-09-23 16:01:00 +0800

    database connection in do

--- src/configuration.rs
+++ src/configuration.rs
@@ -47,6 +47,10 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     settings.merge(
         config::File::from(configuration_directory.join(environment.as_str())).required(true),
     )?;
+
+    // Add in settings from environment variables (with a prefix of APP and '__' as separator)
+    // E.g. `APP_APPLICATION__PORT=5001` would set `Settings.application.port`
+    settings.merge(config::Environment::with_prefix("app").separator("__"))?;
     settings.try_into()
 }
 
