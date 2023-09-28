1800412e5184d96f9e0549c44f2f18a115a7719e
daolanfler <daolanfler@gmail.com>
2023-09-28 16:14:05 +0800

    refactor: eliminate helper 和 main 的重复代码

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,11 +1,9 @@
 use once_cell::sync::Lazy;
-use std::net::TcpListener;
-use zero2prod::email_client::EmailClient;
+use zero2prod::startup::{get_connection_pool, Application};
 
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
-use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 static TRACING: Lazy<()> = Lazy::new(|| {
@@ -26,40 +24,48 @@ pub struct TestApp {
     pub db_pool: PgPool,
 }
 
-pub async fn spawn_app() -> TestApp {
-    Lazy::force(&TRACING);
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-
-    let mut configuration = get_configuration().expect("Failed to read configuration");
-    configuration.database.database_name = Uuid::new_v4().to_string();
-
-    let connection_pool = configure_database(&configuration.database).await;
-
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let server =
-        run(listener, connection_pool.clone(), email_client).expect("Failed to bind address");
-
-    let _ = tokio::spawn(server);
-
-    TestApp {
-        address: format!("http://127.0.0.1:{}", port),
-        db_pool: connection_pool,
+impl TestApp {
+    pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
+        reqwest::Client::new()
+            .post(&format!("{}/subscriptions", &self.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.")
     }
 }
 
+pub async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
+    let configuration = {
+        let mut c = get_configuration().expect("Failed to read configuration");
+
+        c.database.database_name = Uuid::new_v4().to_string();
+        // use random OS port
+        c.application.port = 0;
+
+        c
+    };
+
+    // Create and migrate the database
+    configure_database(&configuration.database).await;
+
+    // Launch the application as a background task
+    let application = Application::build(configuration.clone())
+        .await
+        .expect("Failed to build application.");
+    let address = format!("http:127.0.0.1:{}", application.port());
+    let _ = tokio::spawn(application.run_until_stopped());
+
+    TestApp {
+        address,
+        db_pool: get_connection_pool(&configuration.database),
+    }
+}
+
+/// create and migrate the database
 async fn configure_database(config: &DatabaseSettings) -> PgPool {
     let mut connection = PgConnection::connect_with(&config.without_db())
         .await
