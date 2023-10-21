9ed227b61d8f0452abf1854c96636c98e048b3da
daolanfler <daolanfler@gmail.com>
2023-10-21 22:05:48 +0800

    read cookie

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,8 +1,13 @@
-use actix_web::{get, http::header::ContentType, web, HttpResponse};
+use actix_web::{get, http::header::ContentType, HttpRequest, HttpResponse};
 
 #[get("/login")]
-pub async fn login_form() -> HttpResponse {
-    let error_html = "some html";
+pub async fn login_form(request: HttpRequest) -> HttpResponse {
+    let error_html = match request.cookie("_flash") {
+        None => "".into(),
+        Some(cookie) => {
+            format!("<p><i>{}</i></p>", cookie.value())
+        }
+    };
     HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(format!(
